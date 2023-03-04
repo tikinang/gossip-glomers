0db2ebc3f0ed961f70b8f90b2a89083c37ad074f
Matěj Pavlíček <tikinang@gmail.com>
2023-03-04 19:05:30 +0100

    3c: maelstrom-broadcast

--- maelstrom-broadcast/main.go
+++ maelstrom-broadcast/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
-	"log"
 	"sync"
+	"sync/atomic"
+	"time"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
@@ -41,7 +43,10 @@ func main() {
 		return nil
 	})
 
-	distribute := make(chan Gossip, 64)
+	q := &queue{
+		mut:  new(sync.Mutex),
+		vals: make(map[uint64]Gossip, 1024),
+	}
 	n.Handle("broadcast", func(msg maelstrom.Message) error {
 		var in BroadcastMessageBody
 		if err := json.Unmarshal(msg.Body, &in); err != nil {
@@ -49,11 +54,11 @@ func main() {
 		}
 
 		store.insert(in.Message)
-		distribute <- Gossip{
+		q.push(Gossip{
 			Type:    "gossip",
 			Message: in.Message,
 			SentTo:  []string{selfId},
-		}
+		})
 
 		out := map[string]any{
 			"type": "broadcast_ok",
@@ -68,11 +73,11 @@ func main() {
 		}
 
 		store.insert(in.Message)
-		distribute <- Gossip{
+		q.push(Gossip{
 			Type:    "gossip",
 			Message: in.Message,
 			SentTo:  append(in.SentTo, selfId),
-		}
+		})
 
 		return nil
 	})
@@ -92,30 +97,88 @@ func main() {
 		return n.Reply(msg, out)
 	})
 
+	closed := new(atomic.Bool)
+
 	wg := new(sync.WaitGroup)
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		for gossip := range distribute {
-			for _, fellowId := range fellowIds {
-				var alreadyReceived bool
-				for _, nodeId := range gossip.SentTo {
-					if nodeId == fellowId {
-						alreadyReceived = true
-						break
-					}
-				}
-				if !alreadyReceived {
-					_ = n.Send(fellowId, gossip)
-				}
+		for {
+			gossips := q.get()
+			if closed.Load() && len(gossips) == 0 {
+				break
 			}
+			wg := new(sync.WaitGroup)
+			for message, gossip := range gossips {
+				wg.Add(1)
+				go func(message uint64, gossip Gossip) {
+					defer wg.Done()
+					failed := new(atomic.Bool)
+					wg := new(sync.WaitGroup)
+					for _, fellowId := range fellowIds {
+						var alreadyReceived bool
+						for _, nodeId := range gossip.SentTo {
+							if nodeId == fellowId {
+								alreadyReceived = true
+								break
+							}
+						}
+						if alreadyReceived {
+							continue
+						}
+						wg.Add(1)
+						go func(fellowId string, gossip Gossip) {
+							defer wg.Done()
+							ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+							defer cancel()
+							if _, err := n.SyncRPC(ctx, fellowId, gossip); err != nil {
+								failed.Store(true)
+							}
+						}(fellowId, gossip)
+					}
+					wg.Wait()
+					// This could be optimized. Only try to resent failed requests.
+					if !failed.Load() {
+						q.ok(message)
+					}
+				}(message, gossip)
+			}
+			wg.Wait()
 		}
 	}()
 
 	if err := n.Run(); err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-	close(distribute)
+	closed.Store(true)
+	wg.Wait()
+}
+
+type queue struct {
+	mut  *sync.Mutex
+	vals map[uint64]Gossip
+}
+
+func (r *queue) get() map[uint64]Gossip {
+	r.mut.Lock()
+	defer r.mut.Unlock()
+	m := make(map[uint64]Gossip, len(r.vals))
+	for message, gossip := range r.vals {
+		m[message] = gossip
+	}
+	return m
+}
+
+func (r *queue) ok(message uint64) {
+	r.mut.Lock()
+	defer r.mut.Unlock()
+	delete(r.vals, message)
+}
+
+func (r *queue) push(gossip Gossip) {
+	r.mut.Lock()
+	defer r.mut.Unlock()
+	r.vals[gossip.Message] = gossip
 }
 
 type safeMap struct {
